9b685df3da34ea7bdb7089cbeed97cfb04b3f063
João Pitoli <joao.contats@gmail.com>
2023-01-23 10:26:49 -0300

    novo site

---
+++ contato-patrick.js
@@ -0,0 +1,10 @@
+function Enviar(){
+    var nome = document.getElementById("nomeid");
+    if(nome.value !=""){
+        alert(nome.value + '?');
+    }
+}
+
+function redirecionar() {
+    window.location.href = "pagina-patrick.html";
+}
